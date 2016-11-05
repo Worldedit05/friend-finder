10b146883a8a61383b800b73b3cf71926860843a
Aaron Lee <worldedit05@gmail.com>
2016-11-04 21:43:16 -0500

    Add friend array

---
+++ app/data/friends.js
@@ -0,0 +1,9 @@
+//---------------------------------------------\
+// DATA
+// Will hold all the friend data
+//---------------------------------------------
+
+var friendArray = [];
+
+// Export the arrays
+module.exports = friendArray;
