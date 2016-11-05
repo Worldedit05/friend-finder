a7ded229b32d3558ff57156d0475ca4a1110ba41
Aaron Lee <worldedit05@gmail.com>
2016-11-05 17:45:37 -0500

    POST returns the match

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -19,7 +19,41 @@ module.exports = function (app) {
 
     // API POST Requests
     app.post('/api/friends', function (req, res) {
-        friendData.push(req.body);
-        res.json(true);
+
+        var resultsArry = [];
+        newUserData = req.body;
+
+        friendData.forEach(function(element, index) {
+
+          var newArry = element.scores;
+          var operationArry = [];
+          var evaluation = 0;
+
+          newArry.forEach(function(element, index) {
+            var result = parseInt(newArry[index]) - parseInt(newUserData.scores[index]);
+            operationArry.push(Math.abs(result));
+          });
+
+          operationArry.forEach(function(element) {
+            evaluation += parseInt(element);
+          });
+          resultsArry.push(evaluation);
+        });
+
+        console.log(resultsArry);
+
+        // Need to find the lowest number in the results arrays
+
+        var lowestNumber = Math.min.apply(null, resultsArry);
+
+        // Once we have the lowest number, we need to locate the index to match it with the friendData to find the match
+
+        var index = resultsArry.indexOf(lowestNumber);
+
+        // Send the match as the result of the POST
+
+        res.json(friendData[index]);
+
+        friendData.push(newUserData);
     });
 };
